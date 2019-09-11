2d4afc8be7b34b654405811e5e16098cd27ae4bb
minjoo <crocess@ajou.ac.kr>
2019-09-11 22:12:45 +0900

    ADD FLArticle ADD FLArticle stories

---
+++ components/FLArticle/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>
+        {{title}}
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: 'front-line-article',
+        props: {
+            title: String,
+        }
+    }
+</script>
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,33 +1,32 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import {storiesOf} from '@storybook/vue';
+// import {action} from '@storybook/addon-actions';
+// import {linkTo} from '@storybook/addon-links';
 
-import MyButton from './MyButton';
-import Welcome from './Welcome';
+import Article from '../components/FLArticle'
 
-storiesOf('Welcome', module).add('to Storybook', () => ({
-  components: { Welcome },
-  template: '<welcome :showApp="action" />',
-  methods: { action: linkTo('Button') },
+storiesOf('CMS', module).add('article', () => ({
+    components: {Article},
+    template: '<article title="hi"/>',
+    // methods: { action: linkTo('Button') },
 }));
 
-storiesOf('Button', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    render(h) {
-      return '<my-button @click="action">With JSX</my-button>';
-    },
-    methods: { action: linkTo('clicked') },
-  }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
-  }));
+// storiesOf('Button', module)
+//   .add('with text', () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">Hello Button</my-button>',
+//     methods: { action: action('clicked') },
+//   }))
+//   .add('with JSX', () => ({
+//     components: { MyButton },
+//     render(h) {
+//       return '<my-button @click="action">With JSX</my-button>';
+//     },
+//     methods: { action: linkTo('clicked') },
+//   }))
+//   .add('with some emoji', () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+//     methods: { action: action('clicked') },
+//   }));
